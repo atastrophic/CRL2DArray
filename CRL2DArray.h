1a399138cbf639956073f48899761fc92604c2ab
Ali Turab Gilani <mail.tgilani@googlemail.com>
2013-01-21 12:11:20 +0500

    Commiting Files

---
+++ CRL2DArray.h
@@ -0,0 +1,18 @@
+//
+//  CRLTwoDArray.h
+//  JewelnJewels
+//
+//  Created by tGilani on 20/11/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRL2DArray : NSObject
+
+- (id) initWithRows:(int)x columns:(int)y;
+- (BOOL) insertObject:(id)data atRow:(int)x column:(int)y;
+- (id) objectAtRow:(int)x column:(int)y;
+- (void) removeAllObjects;
+
+@end
---
+++ CRL2DArray.m
@@ -0,0 +1,79 @@
+//
+//  CRLTwoDArray.m
+//  JewelnJewels
+//
+//  Created by tGilani on 20/11/2012.
+//
+//
+
+#import "CRL2DArray.h"
+@interface CRL2DArray ()
+{
+    int capacity;
+}
+@property (nonatomic, retain) NSMutableArray *outerArray;
+@end
+
+@implementation CRL2DArray
+@synthesize outerArray;
+
+- (id) initWithRows:(int)x columns:(int)y
+{
+    if (self = [super init])
+    {
+        capacity = y;
+        self.outerArray = [NSMutableArray array];
+        for (int i = 0; i < x; i++) {
+            NSMutableArray *innerArray = [NSMutableArray array];
+            for (int j = 0; j < y; j++) {
+                [innerArray addObject:@""];
+            }
+            [self.outerArray addObject:innerArray];
+        }
+    }
+    return self;
+}
+
+- (BOOL) insertObject:(id)data atRow:(int)x column:(int)y
+{
+    if (x < outerArray.count) {
+        NSMutableArray *array = [outerArray objectAtIndex:x];
+        if (y < capacity) {
+            if (data == nil)
+                [array replaceObjectAtIndex:y withObject:@""];
+            else
+                [array replaceObjectAtIndex:y withObject:data];
+            return YES;
+        }
+    }
+    [NSException raise:@"Index out of Bounds" format:@"The Indices Provided were Incorrect %i, %i", x,y];
+    return NO;
+}
+
+- (id) objectAtRow:(int)x column:(int)y
+{
+    if (x < outerArray.count) {
+        NSMutableArray *array = [outerArray objectAtIndex:x];
+        if (y < capacity) {
+            id data = (id)[array objectAtIndex:y];
+            if ([data isKindOfClass:[NSString class]])
+                return nil;
+            else
+                return data;
+        }
+    }
+    
+    [NSException raise:@"Index out of Bounds" format:@"The Indices Provided were Incorrect %i, %i", x,y];
+    return nil;
+}
+
+- (void) removeAllObjects
+{
+    [self.outerArray removeAllObjects];
+}
+- (void)dealloc
+{
+    [outerArray release];
+    [super dealloc];
+}
+@end
